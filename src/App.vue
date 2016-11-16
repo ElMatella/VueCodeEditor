b4507226b9ed9001a232d22ac141c01f57d1dd16
Mathieu Marteau <marteau.mathieu@gmail.com>
2016-11-16 16:48:30 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <code-area></code-area>
+  </div>
+</template>
+
+<script>
+import CodeArea from './components/CodeArea.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CodeArea
+  }
+}
+</script>
---
+++ src/components/CodeArea.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="code-area">
+		<pre class="highlighted-code" v-html="highlightedCode.value"></pre>
+		<textarea v-model="localValue"></textarea>
+	</div>
+</template>
+
+<script>
+	import php from 'highlight.js/lib/languages/php'
+	import hljs from 'highlight.js/lib/highlight'
+	
+
+	export default {
+		data () {
+			return {
+				localValue: ''
+			}
+		},
+		computed: {
+			highlightedCode () {
+				return hljs.highlightAuto(this.localValue,['php'])
+			}
+		},
+		created () {
+			hljs.registerLanguage('php',php)
+			console.log(hljs.listLanguages())
+		}
+	}
+</script>
+
+<style type="text/css" scoped>
+	.code-area {
+		//background: grey;
+		position: relative;
+		height: 300px;
+	}
+	textarea {
+		display: block;
+		width: 100%;
+		height: 300px;
+		background: transparent;
+		padding: 0;
+		border: none;
+		position: absolute;
+		-webkit-text-fill-color: transparent;
+	}
+	textarea:focus {
+		outline: none;
+	}
+	.highlighted-code {
+		position: absolute;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		top: 0;
+		margin: 0;
+	}
+</style>
